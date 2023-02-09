a2451ec322c3b01ffeb58956f0f8f27491f851ff
nitinvi-dev <nitinvi.dev@gmail.com>
2023-02-09 16:34:59 +0530

    revert chaqnges and set dynamic parks

--- src/component/Map.js
+++ src/component/Map.js
@@ -3,83 +3,64 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import locationIcon from "../contants/icon";
 import parkIcon from "../contants/parkicon";
-import park from "../contants/parks.json";
-import L from "leaflet";
 
 export default function Map() {
+  const [currentPosition, setcurrentPosition] = useState(null);
+  const [parkPosition, setparkPosition] = useState(null);
 
-  const [positions, setPosition] = useState(park["1000"]);
+  function ParkMarker() {
+    return parkPosition === null
+      ? null
+      : parkPosition.map((val, index) => (
+          <Marker key={index} position={val[0]} icon={parkIcon}>
+            <Popup>
+              <div dangerouslySetInnerHTML={{ __html: val[1] }} />
+            </Popup>
+          </Marker>
+        ));
+  }
 
-  const [currentPosition, setCurrentPosition] = useState(['33.8670522', '151.1957362']);
-
-  function ParkMarker() {    
-    return (
-      positions.map((val, index) => (
-        <Marker key={index} position={val[0]} icon={parkIcon}>
-          <Popup>
-          <div dangerouslySetInnerHTML={{__html: val[1]}} />
-          </Popup>            
-        </Marker>
-      ))
-    );
-  }  
-
-  function LocationMarker() {    
-    const [box, setBbox] = useState([]);
-
+  const LocationMarker = () => {
     const map = useMap();
-
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
-        setCurrentPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const radius = e.accuracy;
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
+        setcurrentPosition([e.latlng.lat, e.latlng.lng]);
       });
-    }, [map]);
+    });
 
     return currentPosition === null ? null : (
       <>
         <Marker position={currentPosition} icon={locationIcon}>
-          <Popup>
-            You are here
-          </Popup>
+          <Popup>You are here</Popup>
         </Marker>
         <ParkMarker />
       </>
     );
-  }
+  };
 
   const findParks = async (e) => {
-    try {
-      const radius = e.target.value;
-      const lat = currentPosition.lat;
-      const lng = currentPosition.lng;
-      const baseUrl = `https://react-leaflet-backend.vercel.app/api/nearbyparks/`;
+    let headers = new Headers();
 
-      const response = await fetch(baseUrl, {
-        method: 'POST',
-        data: {
-          'radius': radius,
-          'lat': lat,
-          'lng': lng
-        },
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Headers': 'application/json'
-        }
-      }).then(res => {
-        return res.json();
-      });
-      console.log('response', response.data);
-      setPosition(response.data);
-    } catch (error) {
-      console.log('=======error', error);
-      setPosition(park[e.target.value]);
-    }
+    headers.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+      radius: e.target.value,
+      lat: String(currentPosition[0]),
+      lng: String(currentPosition[1]),
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: body,
+    };
+
+    fetch("https://react-leaflet-backend.vercel.app/api/nearbyparks", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setparkPosition(result.data);
+      })
+      .catch((error) => console.log("error", error));
   };
 
   return (
@@ -88,15 +69,18 @@ export default function Map() {
         <div className="start">Find parks</div>
         <div className="end">
           <select className="custom-select" onChange={findParks}>
+            <option value="" hidden disabled selected>
+              Please Select Radius
+            </option>
             <option value="1000">1 KM</option>
+            <option value="3000">3 KM</option>
             <option value="5000">5 KM</option>
-            <option value="10000">10 KM</option>
           </select>
         </div>
       </header>
 
       <MapContainer
-        center={ currentPosition }
+        center={[23.0208237, 72.5086395]}
         zoom={13}
         scrollWheelZoom
         style={{ height: "100vh" }}
