86751bfac4086189c759850e202d184b1cbaf138
nitinvi-dev <nitinvi.dev@gmail.com>
2023-02-09 12:41:30 +0530

    backend endpoint set

--- src/component/Map.js
+++ src/component/Map.js
@@ -4,16 +4,17 @@ import "leaflet/dist/leaflet.css";
 import locationIcon from "../contants/icon";
 import parkIcon from "../contants/parkicon";
 import park from "../contants/parks.json";
-
+import L from "leaflet";
 
 export default function Map() {
 
-  const currentPosition = [23.0208237, 72.5086395];
-  const [parkPostion, setparkPostion] = useState(park["1000"]);
+  const [positions, setPosition] = useState(park["1000"]);
+
+  const [currentPosition, setCurrentPosition] = useState(['33.8670522', '151.1957362']);
 
   function ParkMarker() {    
     return (
-      parkPostion.map((val, index) => (
+      positions.map((val, index) => (
         <Marker key={index} position={val[0]} icon={parkIcon}>
           <Popup>
           <div dangerouslySetInnerHTML={{__html: val[1]}} />
@@ -23,11 +24,19 @@ export default function Map() {
     );
   }  
 
-  const LocationMarker = () => {
+  function LocationMarker() {    
+    const [box, setBbox] = useState([]);
+
     const map = useMap();
+
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
-        map.flyTo(currentPosition, map.getZoom());      
+        setCurrentPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
       });
     }, [map]);
 
@@ -44,7 +53,33 @@ export default function Map() {
   }
 
   const findParks = async (e) => {
-    setparkPostion(park[e.target.value]);
+    try {
+      const radius = e.target.value;
+      const lat = currentPosition.lat;
+      const lng = currentPosition.lng;
+      const baseUrl = `https://react-leaflet-backend.vercel.app/api/nearbyparks/`;
+
+      const response = await fetch(baseUrl, {
+        method: 'POST',
+        data: {
+          'radius': radius,
+          'lat': lat,
+          'lng': lng
+        },
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Headers': 'application/json'
+        }
+      }).then(res => {
+        return res.json();
+      });
+      console.log('response', response.data);
+      setPosition(response.data);
+    } catch (error) {
+      console.log('=======error', error);
+      setPosition(park[e.target.value]);
+    }
   };
 
   return (
@@ -61,7 +96,7 @@ export default function Map() {
       </header>
 
       <MapContainer
-        center={ [23.0208237, 72.5086395] }
+        center={ currentPosition }
         zoom={13}
         scrollWheelZoom
         style={{ height: "100vh" }}
